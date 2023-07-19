2cbf72c3ca660aaaecdd58b19cb43a312864292a
Linh Nguyen <linhnguyen.0226@gmail.com>
2023-07-19 23:35:13 +0700

    - Add homepage

--- app/src/main/java/com/example/coffee_order_app/MainActivity.kt
+++ app/src/main/java/com/example/coffee_order_app/MainActivity.kt
@@ -1,11 +1,22 @@
 package com.example.coffee_order_app
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.splash_screen)
+
+        supportActionBar?.hide()
+
+        Handler(Looper.getMainLooper()).postDelayed({
+            var intent = Intent(this@MainActivity, HomeActivity::class.java)
+            startActivity(intent)
+            finish()
+        }, 2000)
     }
 }
