6f4b8668bf42da070ebbb5a14c24359b6039a2cf
Linh Nguyen <linhnguyen.0226@gmail.com>
2023-07-22 23:34:51 +0700

    - Add animation for fragment transition

--- app/src/main/java/com/example/coffee_order_app/DetailsFragment.kt
+++ app/src/main/java/com/example/coffee_order_app/DetailsFragment.kt
@@ -2,12 +2,15 @@ package com.example.coffee_order_app
 
 import android.os.Bundle
 import android.telecom.Call.Details
+import android.transition.TransitionInflater
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import androidx.activity.OnBackPressedCallback
 import androidx.core.content.ContextCompat
+import androidx.core.view.ViewCompat
 import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.fragment.findNavController
 import com.example.coffee_order_app.databinding.ActivityHomeBinding
@@ -22,6 +25,12 @@ class DetailsFragment : Fragment() {
     // This property is only valid between onCreateView and
     // onDestroyView.
     private val binding get() = _binding!!
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        sharedElementEnterTransition = TransitionInflater.from(context).inflateTransition(android.R.transition.move)
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -44,5 +53,7 @@ class DetailsFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        val heroImageView = view.findViewById<ImageView>(R.id.details_image)
+        ViewCompat.setTransitionName(heroImageView, "hero_image")
     }
 }
